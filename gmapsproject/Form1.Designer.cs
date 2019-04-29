932b98a7c845f49e1754b9d13378ac663291dd6d
Musa Yagci <musa.yagci@stud.vistula.edu.pl>
2019-04-28 20:57:56 -0700

    Add project files.

---
+++ gmapsproject/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace gmapsproject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.gmap = new GMap.NET.WindowsForms.GMapControl();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_Distance = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // gmap
+            // 
+            this.gmap.Bearing = 0F;
+            this.gmap.CanDragMap = true;
+            this.gmap.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gmap.GrayScaleMode = false;
+            this.gmap.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gmap.LevelsKeepInMemmory = 5;
+            this.gmap.Location = new System.Drawing.Point(-2, -1);
+            this.gmap.MarkersEnabled = true;
+            this.gmap.MaxZoom = 18;
+            this.gmap.MinZoom = 0;
+            this.gmap.MouseWheelZoomEnabled = true;
+            this.gmap.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gmap.Name = "gmap";
+            this.gmap.NegativeMode = false;
+            this.gmap.PolygonsEnabled = true;
+            this.gmap.RetryLoadTile = 0;
+            this.gmap.RoutesEnabled = true;
+            this.gmap.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gmap.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gmap.ShowTileGridLines = false;
+            this.gmap.Size = new System.Drawing.Size(566, 368);
+            this.gmap.TabIndex = 0;
+            this.gmap.Zoom = 5D;
+            this.gmap.Load += new System.EventHandler(this.gmap_Load);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(570, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(352, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Distance between Berlin and Moscow is:";
+            // 
+            // textBox_Distance
+            // 
+            this.textBox_Distance.Location = new System.Drawing.Point(574, 87);
+            this.textBox_Distance.Name = "textBox_Distance";
+            this.textBox_Distance.Size = new System.Drawing.Size(100, 22);
+            this.textBox_Distance.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(994, 371);
+            this.Controls.Add(this.textBox_Distance);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gmap);
+            this.Name = "Form1";
+            this.Text = resources.GetString("$this.Text");
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private GMap.NET.WindowsForms.GMapControl gmap;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_Distance;
+    }
+}
+
---
+++ gmapsproject/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GMap.NET;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using GMap.NET.MapProviders;
+
+namespace gmapsproject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            gmap.MapProvider = GMap.NET.MapProviders.GoogleMapProvider.Instance;
+            GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerOnly;
+            gmap.Position = new GMap.NET.PointLatLng(52.520008, 13.404954);
+            gmap.ShowCenter = false;
+
+            GMapOverlay markers = new GMapOverlay("markers");
+            GMapMarker marker = new GMarkerGoogle(
+                new PointLatLng(52.520008, 13.404954),
+                GMarkerGoogleType.blue_pushpin);
+            markers.Markers.Add(marker);
+            gmap.Overlays.Add(markers);
+
+        }
+
+        private void gmap_Load(object sender, EventArgs e)
+        {
+            GMapOverlay routes = new GMapOverlay("routes");
+            List<PointLatLng> points = new List<PointLatLng>();
+            points.Add(new PointLatLng(52.520008, 13.404954));
+            points.Add(new PointLatLng(55.751244, 37.618423));
+            GMapRoute route = new GMapRoute(points, "A walk in the park");
+            route.Stroke = new Pen(Color.Red, 3);
+            routes.Routes.Add(route);
+            gmap.Overlays.Add(routes);
+
+            textBox_Distance.Text = route.Distance + " km";
+        }
+    }
+}
